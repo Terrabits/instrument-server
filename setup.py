125fabb2345ab714ab2d0ee72d776eea74c3853f
Nick Lalic <nick.lalic@gmail.com>
2019-09-30 11:56:02 -0700

    🔖 v1.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='instrument-server',
-    version='1.1.1',
+    version='1.2.0',
     description='TCP server for controlling multiple instruments with a simplified SCPI interface',
     long_description=open('README.md').read().strip(),
     author='Nick Lalic',
