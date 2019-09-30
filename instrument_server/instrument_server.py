6867e0aa7b65bf58ff2a6cad34aa6e2ead9e559b
Nick Lalic <nick.lalic@gmail.com>
2019-09-30 11:54:16 -0700

    🐛 Handle IPv6 correctly.

--- instrument_server/instrument_server.py
+++ instrument_server/instrument_server.py
@@ -1,6 +1,10 @@
 from   .server import Server
 import asyncio
 from   asyncio import start_server
+from   socket  import AddressFamily
+
+def ipv4_sockets_from(sockets):
+    return [i for i in sockets if i.family != AddressFamily.AF_INET6]
 
 def create_handler(server):
     async def handler(reader, writer):
@@ -18,8 +22,13 @@ def run(address='0.0.0.0', port=None, *args, **kwargs):
         server       = Server(*args, **kwargs)
         handler      = create_handler(server)
         tcp_server   = await start_server(handler, address, port)
-        _addr, _port = tcp_server.sockets[0].getsockname()
-        print(f'Running on {_addr}:{_port}...')
+        ipv4_sockets = ipv4_sockets_from(tcp_server.sockets)
+        if ipv4_sockets:
+            _addr, _port = tcp_server.sockets[0].getsockname()
+            print(f'Running on {_addr}:{_port}...')
+        else:
+            sockname = tcp_server.sockets[0].getsockname()
+            print(f'Running on {sockname}')
         async with tcp_server:
             await tcp_server.serve_forever()
     try:
