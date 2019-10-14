883b41582870e2955c7d29acb703b875d7794e8d
Nick Lalic <nick.lalic@gmail.com>
2019-10-13 21:49:04 -0700

    ♻💩 Used classifiers from rohdeschwarz.

--- setup.py
+++ setup.py
@@ -18,7 +18,36 @@ setup(
     license='R&S Terms and Conditions for Royalty-Free Products',
     zip_safe=False,
     keywords='RF instrument SCPI',
-    classifiers=['Packages', 'RF', 'instrument', 'SCPI'],
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Environment
+        'Environment :: Console',
+
+        # OS
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: Other/Proprietary License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
     entry_points={
     'console_scripts': [
         'instrument-server=instrument_server.bin.instrument_server:main'
