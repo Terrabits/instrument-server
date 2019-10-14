10f4ee671ff3ec8f3bbde59554a2a527921f6416
Nick Lalic <nick.lalic@gmail.com>
2019-10-13 21:39:30 -0700

    ♻️  Included github url.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     long_description=open('README.md').read().strip(),
     author='Nick Lalic',
     author_email='nick.lalic@gmail.com',
-    url='http://path-to-my-packagename',
+    url='https://github.com/Terrabits/instrument-server',
     py_modules=['instrument_server'],
     packages=find_packages(exclude=['test']),
     install_requires=['ruamel.yaml>=0.15.85<1.0', 'pyvisa>=1.9.1<2.0'],
@@ -15,10 +15,10 @@ setup(
     'dev':  ['ddt>=1.2.0<2.0'],
     'test': ['ddt>=1.2.0<2.0']
     },
-    license='MIT License',
+    license='R&S Terms and Conditions for Royalty-Free Products',
     zip_safe=False,
     keywords='RF instrument SCPI',
-    classifiers=['Packages', 'RF'],
+    classifiers=['Packages', 'RF', 'instrument', 'SCPI'],
     entry_points={
     'console_scripts': [
         'instrument-server=instrument_server.bin.instrument_server:main'
