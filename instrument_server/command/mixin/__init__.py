11ebe10d359be465029a6700bfad47ad31af7f5a
Nick Lalic <nick.lalic@gmail.com>
2022-03-29 15:17:37 -0600

    🏗  parser_error should be exported from module

--- instrument_server/command/mixin/__init__.py
+++ instrument_server/command/mixin/__init__.py
@@ -1,2 +1,4 @@
 from .error_queue import ErrorQueueMixin
-from .parser      import ParserMixin
+from .parser      import parser_error, ParserMixin
+
+__all__ = ['ErrorQueueMixin', 'parser_error', 'ParserMixin']
